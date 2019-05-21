604f565f33c7bf54daca2a4c10e46c961e5e62e5
Sepideh Abadpour <abadpurs@gmail.com>
2019-05-22 01:04:11 +0430

    Tiny modification in Affine_2D_ParameterCalculation.m

--- MATLAB/Affine_2D_ParameterCalculation.m
+++ MATLAB/Affine_2D_ParameterCalculation.m
@@ -1,5 +1,5 @@
 function Parameters = Affine_2D_ParameterCalculation(x_input,y_input,X_output,Y_output)
-% Calculates parameters X=[a1;a2;a0;b1,b2;b0] in the following equation using least squares
+% Calculates parameters X=[a1;a2;a0;b1;b2;b0] in the following equation using least squares
 %X=a1x+a2y+a0
 %Y=b1x+b2y+b0
 % x_input is a Nx1 matrix containing x coordinate of control points in the input space
